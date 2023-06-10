1ab7d43e0b75aec0c5b2d345c1de8fad4255e414
MITHUN TR <tr7mithun@gmail.com>
2023-06-10 22:17:31 +0530

    Added form elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Header from './Components/Header'
+import Meme from './Components/Meme'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <Meme />
     </>
   )
 }
---
+++ src/Components/Meme.jsx
@@ -0,0 +1,23 @@
+const Meme = () => {
+    return (
+
+        <>
+            <section className="form-section">
+                <form className="form">
+
+                    <input type="text" placeholder="Top Text" />
+                    <input type="text" placeholder="Bottom Text" />
+                    <button>Get a new meme image  &#128444;&#65039;</button>
+
+
+                </form>
+            </section>
+
+
+        </>
+
+
+    );
+}
+
+export default Meme;
